f7ccc685129e8d7b05584fe5c83de60374ba5045
fakeaccount12 <61002152+fakeaccount12@users.noreply.github.com>
2020-02-13 15:47:07 +0700

    Update ota.mk

--- config/ota.mk
+++ config/ota.mk
@@ -3,12 +3,12 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 CUSTOM_OTA_VERSION_CODE := android_10
 
 CUSTOM_PROPERTIES += \
-    org.extendedui.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
+    org.kangui.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
 
 #PRODUCT_PACKAGES += \
 #    Updates
 
 PRODUCT_COPY_FILES += \
-    vendor/aosp/config/permissions/org.extendedui.ota.xml:system/etc/permissions/org.extendedui.ota.xml
+    vendor/aosp/config/permissions/org.kangui.ota.xml:system/etc/permissions/org.kangui.ota.xml
 
 endif
